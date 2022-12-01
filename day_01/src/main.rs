918a167f039a5d0d25622930d021744097035c76
jurgob <jurgob@users.noreply.github.com>
2022-12-01 17:04:30 +0000

    solution pt 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,70 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+
+fn part_1(input: &String) -> i32{
+
+    let lines = input.lines();
+    let mut res = 0;
+    let mut cur_elf_calories = 0;
+    for line in lines {
+
+        if line.trim().is_empty() {
+            if cur_elf_calories > res  { res = cur_elf_calories}
+
+            cur_elf_calories = 0;
+
+        } else {
+            let calories = line.parse::<i32>().unwrap();
+            cur_elf_calories = cur_elf_calories + calories;
+        }
+
+
+    }
+    
+    res
+    
+
+}
+
+fn part_2(input: &String) -> i32{
+
+    let lines = input.lines();
+    let mut res = 0;
+    let mut cur_elf_calories = 0;
+    for line in lines {
+
+        if line.trim().is_empty() {
+            if cur_elf_calories > res  { res = cur_elf_calories}
+
+            cur_elf_calories = 0;
+
+        } else {
+            let calories = line.parse::<i32>().unwrap();
+            cur_elf_calories = cur_elf_calories + calories;
+        }
+
+
+    }
+    
+    res
+    
+
+}
+
+// pt 2 
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("./input.txt")
+        .expect("File not found");
+
+    let mut input = String::new();
+
+    file.read_to_string(&mut input)
+        .expect("Error while reading file");
+
+    println!("Part 1 solution: {}", part_1(&input));
+    println!("Part 2 solution: {}", part_2(&input));
+
+    
+
 }
