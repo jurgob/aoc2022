9e6cd8ff33d325c97e46ad8b77e131c7372cedde
jurgob <jurgob@users.noreply.github.com>
2022-12-02 14:59:08 +0000

    day 2, pt 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -6,17 +6,21 @@ The score for a single round is the score for the shape you selected
     Scissors (C,Z) => 3
 
 Outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
+
+pt2: 
+X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+
  */
 fn match_score(s: &str) -> Result<i32, ()> {
     match s {
-        // drow
-        "AX" => Ok(3),
-        "BY" => Ok(3),
-        "CZ" => Ok(3),
         //p1 win
         "AZ" => Ok(0),
         "BX" => Ok(0),
         "CY" => Ok(0),
+        // drow
+        "AX" => Ok(3),
+        "BY" => Ok(3),
+        "CZ" => Ok(3),
         //p2 win
         "CX" => Ok(6),
         "AY" => Ok(6),
@@ -34,6 +38,25 @@ fn figure_score(s: &char) -> Result<i32, ()> {
     }
 } 
 
+
+fn translate(s: &str) -> Result<&str,()> {
+    match s {
+        // p1 win (you loose)
+        "AX" => Ok("AZ"),
+        "BX" => Ok("BX"),
+        "CX" => Ok("CY"),
+        //draw
+        "AY" => Ok("AX"),
+        "BY" => Ok("BY"),
+        "CY" => Ok("CZ"),
+        //p2 win (you wint)
+        "AZ" => Ok("AY"),
+        "BZ" => Ok("BZ"),
+        "CZ" => Ok("CX"),
+        _ => Err(()),
+    }
+}
+
 fn main() {
     let input = include_str!("../input.txt");
 
@@ -43,6 +66,15 @@ fn main() {
         let score = match_score(&result).unwrap() + figure_score(&my_figure).unwrap() ;
         score
     }).sum(); 
+
+    let result_pt2:i32 = input.lines().map(|res| {
+        let result_not_translated = res.replace(" ", "");
+        let result =translate(&result_not_translated).unwrap(); 
+        let my_figure = result.chars().last().unwrap();
+        let score = match_score(&result).unwrap() + figure_score(&my_figure).unwrap() ;
+        score
+    }).sum(); 
   
-    println!("result: {:?}", result);
+    println!("part 1 result: {:?}", result);
+    println!("part 2 result: {:?}", result_pt2);
 }
