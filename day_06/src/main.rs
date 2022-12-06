a96193bc3f40786b84fd1518447590d3c27cd11c
jurgob <jurgob@users.noreply.github.com>
2022-12-06 12:38:26 +0000

    day 6 pt 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,14 +1,26 @@
 fn main() {
     let input = include_str!("../input.txt");
-    let res_p1= pt_1(input);
-    println!("result pt1: {}", res_p1);
+    println!("result pt1: {}", pt_1(input));
+    println!("result pt2: {}", pt_2(input));
 
 }
 
 fn pt_1(input: &str) -> usize{
+    find_token(input,4)
+}
+
+fn is_key(token: &str) -> bool {
+    let v: Vec<char> = token.chars().collect();
+    let mut y = v.clone();
+    y.sort();
+    y.dedup();
+    v.len() == y.len()
+}
+
+fn find_token(input: &str, token_size:usize) -> usize{
     let input_chars = input.chars();
-    for (i, _c) in input_chars.skip(3).enumerate(){
-        let to = i+4;
+    for (i, _c) in input_chars.skip(token_size-1).enumerate(){
+        let to = i+token_size;
         let token = &input[i..to];
         if is_key(token) {
             return to;
@@ -18,12 +30,8 @@ fn pt_1(input: &str) -> usize{
     return 0
 }
 
-fn is_key(token: &str) -> bool {
-    let v: Vec<char> = token.chars().collect();
-    let mut y = v.clone();
-    y.sort();
-    y.dedup();
-    v.len() == y.len()
+fn pt_2(input: &str) -> usize{
+    find_token(input,14)
 }
 
 
@@ -60,5 +68,58 @@ mod tests {
     #[test]
     fn is_key_works_false(){
         assert_eq!(is_key(&"bvwb"), false);
+    }
+}
+
+#[cfg(test)]
+mod tests_pt1 {
+    use crate::pt_1;
+
+    #[test]
+    fn pt_1_works(){
+        assert_eq!(pt_1(&"bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+    } 
+    
+    #[test]
+    fn pt_1_works_1(){
+        assert_eq!(pt_1(&"nppdvjthqldpwncqszvftbrmjlhg"), 6);
+    }
+    #[test]
+    fn pt_1_works_2(){
+        assert_eq!(pt_1(&"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+    }
+
+    #[test]
+    fn pt_1_works_3(){
+        assert_eq!(pt_1(&"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+}
+
+#[cfg(test)]
+mod tests_pt_2 {
+    use crate::pt_2;
+
+    #[test]
+    fn pt_2_works(){
+        assert_eq!(pt_2(&"mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+    } 
+    
+    #[test]
+    fn pt_2_works_1(){
+        assert_eq!(pt_2(&"bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+    }
+    #[test]
+    fn pt_2_works_2(){
+        assert_eq!(pt_2(&"nppdvjthqldpwncqszvftbrmjlhg"), 23);
+    }
+
+    #[test]
+    fn pt_2_works_3(){
+        assert_eq!(pt_2(&"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+    }
+
+    #[test]
+    fn pt_2_works_4(){
+        assert_eq!(pt_2(&"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
     }
 }
